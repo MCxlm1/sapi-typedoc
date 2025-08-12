d06b8592e55a372eec5d5105733d4bdc62e67e9f
MCxlm1 <115008407+MCxlm1@users.noreply.github.com>
2025-08-12 13:52:40 +0800

    Update World.d.ts

--- translate-pieces/server/classes/World.d.ts
+++ translate-pieces/server/classes/World.d.ts
@@ -2,17 +2,12 @@
 
 /**
  * 表示一个世界。包含了世界的各种状态，即一系列维度以及 Minecraft 的环境。
- * 
- * A class that wraps the state of a world - a set of
- * dimensions and the environment of Minecraft.
  */
 export class World {
     private constructor();
     /**
      * @remarks
-     * Contains a set of events that are applicable to the entirety
-     * of the world.  Event callbacks are called in a deferred
-     * manner. Event callbacks are executed in read-write mode.
+     * 包含适用于整个世界的一组事件。事件回调以延迟方式调用。事件回调以读写模式执行。
      *
      * @earlyExecution
      *
@@ -20,9 +15,7 @@ export class World {
     readonly afterEvents: WorldAfterEvents;
     /**
      * @remarks
-     * Contains a set of events that are applicable to the entirety
-     * of the world. Event callbacks are called immediately. Event
-     * callbacks are executed in read-only mode.
+     * 包含适用于整个世界的一组事件。事件回调立即调用。事件回调以只读模式执行。
      *
      * @earlyExecution
      *
@@ -31,7 +24,7 @@ export class World {
     readonly beforeEvents: WorldBeforeEvents;
     /**
      * @remarks
-     * The game rules that apply to the world.
+     * 应用于世界的.gameRules游戏规则。
      *
      */
     readonly gameRules: GameRules;
@@ -39,36 +32,31 @@ export class World {
     /**
      * @remarks
      * 全局的、唯一的记分板对象。
-     * 
-     * Returns the general global scoreboard that applies to the
-     * world.
      *
      */
     readonly scoreboard: Scoreboard;
     /**
      * @remarks
-     * Returns the manager for {@link Structure} related APIs.
+     * 返回与 {@link Structure} 相关 API 的管理器。
      *
      */
     readonly structureManager: StructureManager;
     /**
      * @beta
      * @remarks
-     * A method that is internal-only, used for broadcasting
-     * specific messages between client and server.
+     * 仅供内部使用的方法，用于在客户端和服务器之间广播特定消息。
      *
      * @worldMutation
      *
      * @param id
-     * The message identifier.
+     * 消息标识符。
      * @param value
-     * The message.
+     * 消息内容。
      */
     broadcastClientMessage(id: string, value: string): void;
     /**
      * @remarks
-     * Clears the set of dynamic properties declared for this
-     * behavior pack within the world.
+     * 清除在此行为包中为世界声明的动态属性集合。
      *
      */
     clearDynamicProperties(): void;
@@ -76,8 +64,7 @@ export class World {
      * @remarks
      * 获取自游戏开始以来流逝的时间（计算公式：`day*24000+daytime`）。
      * 时间的流逝受到游戏规则 `dodaylightcycle` 的影响。
-     * 
-     * Returns the absolute time since the start of the world.
+     *
      * @returns 自游戏开始以来流逝的时间，以刻为单位。
      *
      */
@@ -85,18 +72,16 @@ export class World {
     /**
      * @beta
      * @remarks
-     * The aim-assist presets and categories that can be used in
-     * the world.
+     * 可在世界中使用的瞄准辅助预设和类别。
      *
      */
     getAimAssist(): AimAssistRegistry;
     /**
      * @remarks
      * 获取一个包含了游戏中所有玩家的对象的数组。
-     * 
-     * Returns an array of all active players within the world.
+     *
      * @returns 返回包含了游戏中所有玩家的对象的数组。
-     * @throws This function can throw errors.
+     * @throws 此函数可能抛出错误。
      *
      * {@link CommandError}
      *
@@ -105,103 +90,88 @@ export class World {
     getAllPlayers(): Player[];
     /**
      * @remarks
-     * Returns the current day.
+     * 返回当前日期。
      *
      * @returns
-     * The current day, determined by the world time divided by the
-     * number of ticks per day. New worlds start at day 0.
+     * 当前日期，由世界时间除以每天的刻数确定。新世界从第 0 天开始。
      */
     getDay(): number;
     /**
      * @remarks
-     * Returns the default Overworld spawn location.
+     * 返回默认的主世界出生点位置。
      *
      * @returns
-     * The default Overworld spawn location. By default, the Y
-     * coordinate is 32767, indicating a player's spawn height is
-     * not fixed and will be determined by surrounding blocks.
+     * 默认的主世界出生点位置。默认情况下，Y 坐标为 32767，
+     * 表示玩家的出生高度未固定，将由周围方块决定。
      */
     getDefaultSpawnLocation(): Vector3;
     /**
      * @remarks
-     * Gets the difficulty from the world.
+     * 从世界中获取难度。
      *
      * @returns
-     * Returns the world difficulty.
+     * 返回世界难度。
      */
     getDifficulty(): Difficulty;
     /**
      * @remarks
      * 由 `dimensionId` 获取维度对象。
-     * 
-     * Returns a dimension object.
      *
      * @param dimensionId
      * 要获取的维度的标识符。
-     * 
-     * The name of the dimension. For example, "overworld",
-     * "nether" or "the_end".
+     *
+     * 维度的名称。例如，"overworld"、"nether" 或 "the_end"。
      * @returns
      * 与 `dimensionId` 关联的维度对象。
-     * 
-     * The requested dimension
+     *
+     * 请求的维度
      * @throws
      * 若 `dimensionId` 不与任何维度关联，抛出 `"Dimension '<dimensionId>' is invalid"`。
      *
-     * Throws if the given dimension name is invalid
+     * 如果给定的维度名称无效则抛出异常
      */
     getDimension(dimensionId: string): Dimension;
     /**
      * @remarks
      * 获取由 `identifier` 指定的世界中已定义的动态属性的值。
-     * 
-     * Returns a property value.
      *
      * @param identifier
      * 动态属性的标识符。
-     * 
-     * The property identifier.
      * @returns
      * 返回动态属性 `identifier` 的值。属性的值尚未设定时，返回 `undefined`。
-     * 
-     * Returns the value for the property, or undefined if the
-     * property has not been set.
      * @throws
      * 若并未注册以 `identifier` 为标识符的动态属性，抛出 `"Dynamic Property '<identifier>' is not defined"` 。
-     * 
-     * Throws if the given dynamic property identifier is not
-     * defined.
+     *
+     * 如果给定的动态属性标识符未定义则抛出异常。
      * @seeExample incrementDynamicProperty.ts
      * @seeExample incrementDynamicPropertyInJsonBlob.ts
      */
     getDynamicProperty(identifier: string): boolean | number | string | Vector3 | undefined;
     /**
      * @remarks
-     * Gets a set of dynamic property identifiers that have been
-     * set in this world.
+     * 获取在该世界中已设置的一组动态属性标识符。
      *
      * @returns
-     * A string array of active dynamic property identifiers.
+     * 一组有效的动态属性标识符字符串数组。
      */
     getDynamicPropertyIds(): string[];
     /**
      * @remarks
-     * Gets the total byte count of dynamic properties. This could
-     * potentially be used for your own analytics to ensure you're
-     * not storing gigantic sets of dynamic properties.
+     * 获取动态属性的总字节数。这可能用于你自己的分析，
+     * 以确保你没有存储大量动态属性集合。
      *
      */
     getDynamicPropertyTotalByteCount(): number;
     /**
      * @remarks
-     * Returns an entity based on the provided id.
+     * 根据提供的 ID 返回一个实体。
      *
      * @param id
-     * The id of the entity.
+     * 实体的 ID。
      * @returns
-     * The requested entity object.
+     * 请求的实体对象。
      * @throws
-     * Throws if the given entity id is invalid.
+     * 如果给定的实体 ID 无效则抛出异常。
      */
     getEntity(id: string): Entity | undefined;
     /**
@@ -210,14 +180,14 @@ export class World {
     getLootTableManager(): LootTableManager;
     /**
      * @remarks
-     * Returns the MoonPhase for the current time.
+     * 返回当前时间的月相。
      *
      */
     getMoonPhase(): MoonPhase;
     /**
      * @beta
      * @remarks
-     * Returns a map of pack setting name and value pairs.
+     * 返回一组包设置名称和值对。
      *
      * @earlyExecution
      *
@@ -226,25 +196,21 @@ export class World {
     /**
      * @remarks
      * 列出世界上的玩家，可使用 `options` 指定的实体查询选项对其进行筛选。
-     * 
-     * Returns a set of players based on a set of conditions
-     * defined via the EntityQueryOptions set of filter criteria.
      *
      * @param options
      * 可选的参数，用作于筛选指定条件的玩家。
      *
      * 注意，不能使用接口中的 `type`、`location`、`maxDistance`、`minDistance` 或 `volume` 属性。
-     * 
-     * Additional options that can be used to filter the set of
-     * players returned.
+     *
+     * 用于筛选返回玩家集合的附加选项。
      * @returns
-     * A player array.
+     * 玩家数组。
      * @throws
      * 若向 `options` 传入的对象含有 `type` 属性，抛出 `"command.generic.invalidPlayerType"`。
-     * 
+     *
      * 若向 `options` 传入的对象含有 `location`、`maxDistance`、`minDistance` 或 `volume` 属性，抛出 `"EntityQueryOptions property '<property>' is incompatible with function world.getPlayers"`。
-     * 
-     * Throws if the provided EntityQueryOptions are invalid.
+     *
+     * 如果提供的 EntityQueryOptions 无效则抛出异常。
      *
      * {@link CommandError}
      *
@@ -254,26 +220,20 @@ export class World {
     /**
      * @remarks
      * 返回当前一天中的时间。
-     * 
-     * Returns the time of day.
      *
      * @returns
      * 当前一天中的时间，以刻为单位，为 `0` 至 `24000` 之间的整数。
-     * 
-     * The time of day, in ticks, between 0 and 24000.
      */
     getTimeOfDay(): number;
     /**
      * @remarks
      * 停止正在播放的音乐，并开始向玩家播放指定音乐。播放类别不为音乐的声音项目不会有任何效果。
-     * 
-     * Plays a particular music track for all players.
      *
      * @worldMutation
      *
      * @param trackId 声音项目的标识符，要求声音项目的类别为音乐（`category: music`）。
      * @param musicOptions 可选，指定播放音乐使用的附加参数。
-     * @throws This function can throw errors.
+     * @throws 此函数可能抛出错误。
      *
      * {@link minecraftcommon.PropertyOutOfBoundsError}
      * @seeExample playMusicAndSound.ts
@@ -282,24 +242,20 @@ export class World {
     /**
      * @remarks
      * 将音乐添加到播放列表。如果没有任何正在播放的音乐，将会开始播放音乐。播放列表中的音乐将会按照添加顺序播放（需要更多测试）。
-     * 
-     * Queues an additional music track for players. If a track is
-     * not playing, a music track will play.
      *
      * @worldMutation
      *
      * @param trackId
      * 声音项目的标识符，要求声音项目的类别为音乐（`category: music`）。
-     * 
-     * Identifier of the music track to play.
+     *
+     * 要播放的音乐曲目标识符。
      * @param musicOptions
      * 可选，指定播放音乐使用的附加参数。
-     * 
-     * Additional options for the music track.
-     * @throws
-     * An error will be thrown if volume is less than 0.0.
-     * An error will be thrown if fade is less than 0.0.
      *
+     * 音乐曲目的附加选项。
+     * @throws
+     * 如果音量小于 0.0 则会抛出错误。
+     * 如果淡出时间小于 0.0 则会抛出错误。
      *
      * {@link minecraftcommon.PropertyOutOfBoundsError}
      */
@@ -307,43 +263,40 @@ export class World {
     /**
      * @remarks
      * 向所有玩家广播一条消息。
-     * 
-     * Sends a message to all players.
      *
      * @param message
      * 将要广播的一段消息。
      * 这段消息可能是一段字符串，或者符合 `RawMessage` 接口的对象，或是这两种类型的组合。
-     * 
-     * The message to be displayed.
+     *
+     * 要显示的消息。
      * @throws
-     * 该方法在 `message` 格式不正确时会抛出错误。例如 `score` 的 `name` 为空字符串时。
-     * 
-     * This method can throw if the provided {@link RawMessage} is
-     * in an invalid format. For example, if an empty `name` string
-     * is provided to `score`.
+     * 如果提供的 {@link RawMessage} 格式无效，此方法可能抛出异常。
+     * 例如，如果向 `score` 提供了空的 `name` 字符串。
+     *
+     * 如果提供的 {@link RawMessage} 格式无效则会抛出异常。
+     * 例如，如果向 `score` 提供了空的 `name` 字符串。
      */
     sendMessage(message: (RawMessage | string)[] | RawMessage | string): void;
     /**
      * @remarks
-     * Sets the world time.
+     * 设置世界时间。
      *
      * @worldMutation
      *
      * @param absoluteTime
-     * The world time, in ticks.
+     * 以刻为单位的世界时间。
      */
     setAbsoluteTime(absoluteTime: number): void;
     /**
      * @remarks
-     * Sets a default spawn location for all players.
+     * 为所有玩家设置默认出生点。
      *
      * @worldMutation
      *
      * @param spawnLocation
-     * Location of the spawn point. Note that this is assumed to be
-     * within the overworld dimension.
+     * 出生点位置。注意，这被假定在主世界维度内。
      * @throws
-     * Throws if the provided spawn location is out of bounds.
+     * 如果提供的出生点位置超出边界则抛出异常。
      *
      * {@link Error}
      *
@@ -352,23 +305,22 @@ export class World {
     setDefaultSpawnLocation(spawnLocation: Vector3): void;
     /**
      * @remarks
-     * Sets the worlds difficulty.
+     * 设置世界难度。
      *
      * @worldMutation
      *
      * @param difficulty
-     * The difficulty we want to set the world to.
+     * 我们想要设置的世界难度。
      */
     setDifficulty(difficulty: Difficulty): void;
     /**
      * @beta
      * @remarks
-     * Sets multiple dynamic properties with specific values.
+     * 设置具有特定值的多个动态属性。
      *
      * @param values
-     * A Record of key value pairs of the dynamic properties to
-     * set.
-     * @throws This function can throw errors.
+     * 要设置的动态属性的键值对记录。
+     * @throws 此函数可能抛出错误。
      *
      * {@link minecraftcommon.ArgumentOutOfBoundsError}
      */
@@ -376,26 +328,19 @@ export class World {
     /**
      * @remarks
      * 为世界动态属性 `identifier` 设置一个值。
-     * 
-     * Sets a specified property to a value.
      *
      * @param identifier
      * 动态属性的标识符。
-     * 
-     * The property identifier.
      * @param value
      * 要设定的值，值的类型必须与动态属性注册的类型相同。
-     * 
-     * Data value of the property to set.
      * @throws
      * 若并未注册以 `identifier` 为标识符的动态属性，抛出 `"Dynamic Property '<identifier>' is not defined"`。
-     * 
+     *
      * 若动态属性的类型不符合值的类型，抛出 `"Type mismatch for dynamic property '<identifier>'"`。
-     * 
+     *
      * 若动态属性的类型为字符串，且值在使用 UTF-8 编码后的字节长度大于动态属性所允许的最大长度，抛出 `"Maximum string length exceeded (<length>/<maxLength>) for dynamic property '<identifier>'"`。
-     * 
-     * Throws if the given dynamic property identifier is not
-     * defined.
+     *
+     * 如果给定的动态属性标识符未定义则抛出异常。
      *
      * {@link minecraftcommon.ArgumentOutOfBoundsError}
      * @seeExample incrementDynamicProperty.ts
@@ -404,22 +349,19 @@ export class World {
     setDynamicProperty(identifier: string, value?: boolean | number | string | Vector3): void;
     /**
      * @remarks
-     * Sets the time of day.
+     * 设置一天中的时间。
      *
      * @worldMutation
      *
      * @param timeOfDay
-     * The time of day, in ticks, between 0 and 24000.
+     * 一天中的时间，以刻为单位，介于 0 到 24000 之间。
      * @throws
-     * Throws if the provided time of day is not within the valid
-     * range.
+     * 如果提供的时间不在有效范围内则抛出异常。
      */
     setTimeOfDay(timeOfDay: number | TimeOfDay): void;
     /**
      * @remarks
      * 停止客户端中正在播放的所有音乐曲目（需要更多测试）。
-     * 
-     * Stops any music tracks from playing.
      *
      * @worldMutation
      *
